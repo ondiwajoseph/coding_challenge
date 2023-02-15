b0bc6d3cb486030911f8a6161d998196c9b951b9
ondiwajoseph <okinyi.joseph.ondiwa@gmail.com>
2023-02-15 17:04:03 +0300

    Binary gap code

--- 0-binary_gap.py
+++ 0-binary_gap.py
@@ -1,1 +1,20 @@
 #!/user/bin/python3
+
+def solution(N):
+    binaryN = bin(N)[2:]
+    maxGap = 0
+    minGap = 0
+    for i in binaryN:
+        if i == '1':
+            if minGap > maxGap:
+                maxGap = minGap
+            minGap = 0
+        else:
+            minGap += 1
+    return maxGap
+
+'''An example'''
+print("Binary gap of {}: ".format(529),solution(529))
+print("Binary gap of {}: ".format(221),solution(221))
+print("Binary gap of {}: ".format(80),solution(80))
+
